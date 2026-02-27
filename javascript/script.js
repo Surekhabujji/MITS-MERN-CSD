9a565563ffcb13db9cac3d701d73d3bf37b9f90e
Surekha <surekhasunkara45@gmail.com>
2026-02-27 16:05:26 +0530

    JavaScript has done

---
+++ javascript/script.js
@@ -0,0 +1,71 @@
+// console.log("Hello World!!");
+// //variable we can reinitialize and redeclare also 
+// var a;//global scope
+// a=10;
+// var a;
+// a=20;
+// //reinitialize
+// let b=10;//block scope
+//  b=20;
+//  //can't redeclare or reinitialize
+// const c=30;//block scope
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num=10;
+// var str="surekha";//` ` " " ' '
+// var bool=true;
+// var un;
+// var nu=null;
+// var big=1234567789n;
+// var symbol=Symbol('li');
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof un);
+// console.log(typeof nu);
+// console.log(typeof big);
+// console.log(typeof symbol);
+
+// var arr=[1,2,3,4,5]
+// console.log(typeof arr);
+
+// var Obj={
+//     name:"Surekha",
+//     dept:"DS"
+// }//Object
+// console.log(Obj);
+// console.log(typeof Obj);
+
+//Arithmetic Operator
+var a=10;
+var b="2"; // when you give a char instead of this it showing NaN
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a%b);
+console.log(a**b);
+
+//Relational Operator
+var a=10;
+var b="20";
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);//== is used to check the data type not a value
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>=b);
+
+//logical
+var a=true;
+var b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);
+
+
