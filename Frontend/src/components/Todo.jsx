4d5dda0dc6e905a233f18c4d35e7f8a358ce54ad
Surekha <surekhasunkara45@gmail.com>
2026-03-10 16:07:43 +0530

    Frontend and Backend Connected, MangoDB Atlas Connected in Database

--- Frontend/src/components/Todo.jsx
+++ Frontend/src/components/Todo.jsx
@@ -1,39 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Todo = () => {
     const[task,setTask]=useState('');
-    const[todos,setTodos]=useState([{
-        task:'Read',
-        status:false     // if we place true instead of false in console read will strike out  
-    }])
-    const [edit,setEdit]=useState(null);         
+    const[todos,setTodos]=useState([]);
+    const [edit,setEdit]=useState(null);  
+    const API=`http://localhost:3000`
+
+    const fetchTodo=async()=>{
+        const res = await axios.get(`${API}/api/todo/`);
+        setTodos(res.data);
+    }
+    useEffect(()=>{
+        fetchTodo()
+    },[]);       
     //  instead of null if we place the condition it will change add to update
-    const handleAddOrEdit=(e)=>{
+    const handleAddOrEdit=async(e)=>{
         e.preventDefault();
-        if(edit||edit==0){
-            setTodos(todos.map((todo,index)=>(
-                index===edit?{...todo,task:task}:todo
-            )))
+        if(edit){
+            await axios.put(`${API}/api/todo/update/${edit}`,{task})
             setEdit(null);
 
         }
         else{
-            setTodos([...todos,{
-                task:task,
-                status:false
-            }]);
+            await axios.post(`${API}/api/todo/create`,{task})
         }
         setTask('');
+        fetchTodo();
 
     };
-    const handleDelete=(index)=>{
-        setTodos(todos.filter((_,i)=>index!=i))
+    const handleDelete=async(id)=>{
+        await axios.delete(`${API}/api/todo/delete/${id}`)
+        fetchTodo();
     };
-    const handleToggleStatus=(index)=>{
-        setTodos(todos.map((todo,i)=>(
-            index===i?{...todo,status:!todo.status} : todo
-
-        )))
+    const handleToggleStatus=async(todo)=>{
+        await axios.put(`${API}/api/todo/update/${todo._id}`,{
+            completed:!todo.completed
+        })
+        fetchTodo();
     };
   return (
     <div>
@@ -42,23 +46,23 @@ const Todo = () => {
         placeholder='Enter the task'
         value={task}
         onChange={(e)=>setTask(e.target.value)}/>
-        <button onClick={handleAddOrEdit}>{edit||edit==0?"Update":"Add"}</button>
+        <button onClick={handleAddOrEdit}>{(edit)?"Update":"Add"}</button>
         <br /><br />
         <ul>
-            {todos.map((todo,index)=>(
-                <li key={index}>
+            {todos.map((todo)=>(
+                <li key={todo._id}>
                  <span style={
                     {cursor:"pointer",
-                    textDecoration:todo.status?"line-through":"none"}
+                    textDecoration:todo.completed?"line-through":"none"}
                  }
-                 onClick={()=>handleToggleStatus(index)}
+                 onClick={()=>handleToggleStatus(todo)}
                  >{todo.task}</span>
                  <button onClick={()=>{
                     setTask(todo.task);
-                    setEdit(index);
+                    setEdit(todo._id);
 
                  }}>✏️</button>
-                 <button onClick={()=>handleDelete(index)}>🗑️</button>
+                 <button onClick={()=>handleDelete(todo._id)}>🗑️</button>
                 </li>
             ))}
         </ul>
